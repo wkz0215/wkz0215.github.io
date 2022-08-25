89c3db574ca844402254c034ef46b8fb9d31350c
wkz0215 <1516694146@qq.com>
2022-08-25 20:46:26 +0800

    Update Music Unlock Tool

---
+++ webapp/web-music-unlock/precache-manifest.6852322818e853031064d386e02de145.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acbeb2816866e396242e",
+    "url": "css/app.b72c607c.css"
+  },
+  {
+    "revision": "8d57f12e8cc04b462e3f",
+    "url": "css/chunk-vendors.094863c6.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "891d296cc30cce15e1b635fc090e96b6",
+    "url": "index.html"
+  },
+  {
+    "revision": "2d12777f2703612307ff4a12f1b21899",
+    "url": "ixarea-stats.js"
+  },
+  {
+    "revision": "fd79347bd7c3bb4be90a76dd00a90857",
+    "url": "js/0.163a2ba8.worker.js"
+  },
+  {
+    "revision": "acbeb2816866e396242e",
+    "url": "js/app.dc341494.js"
+  },
+  {
+    "revision": "8d57f12e8cc04b462e3f",
+    "url": "js/chunk-vendors.e75bd7b3.js"
+  },
+  {
+    "revision": "02995355b96ddf2519cd49f8aa73bb46",
+    "url": "loader.js"
+  },
+  {
+    "revision": "523b1a2eae8cb533fa6bd73831308f09",
+    "url": "static/kgm.mask"
+  },
+  {
+    "revision": "cd1d395410107c66b4534ec93f0073d3",
+    "url": "web-manifest.json"
+  }
+]);
